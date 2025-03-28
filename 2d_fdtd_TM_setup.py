aa64c6fdb806b056937a7205a9d99b11efdc0973
RenzhiL <renzhil@usc.edu>
2025-03-28 11:54:09 -0700

    debug attempt (failed)

--- 2d_fdtd_TM_setup.py
+++ 2d_fdtd_TM_setup.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 def main():
     # Simulation parameters
@@ -9,9 +10,8 @@ def main():
     lambda_0 = 600
     lambda_U = 1200
     lambda_L = 900
-    dx = dy = 20
+    dx = dy = 50
     dt = dx / (c0 * np.sqrt(2))
-    poly_deg = 3
 
     # Spatial Grid Definition
     x_min, x_max = -1500, 1500
@@ -20,14 +20,14 @@ def main():
     Nx = int(round((x_max - x_min) / dx)) + 1
     Ny = int(round((y_max - y_min) / dy)) + 1
 
-    nt = 230
+    nt = 50
     x_src, y_src= 0, 0
     x_prob, y_prob = 1000, 0
     i_x_src = int(round((x_src - x_min) / dx))
     i_y_src = int(round((y_src - y_min) / dy))
 
-    i_x_prob = int(round((x_prob - x_min) / dx))
-    i_y_prob = int(round((y_prob - y_min) / dy))
+    # i_x_prob = int(round((x_prob - x_min) / dx))
+    # i_y_prob = int(round((y_prob - y_min) / dy))
 
     omega_0 = 2 * np.pi * c0 / lambda_0
     sigma = (2 / omega_0) * (lambda_0 / (lambda_U - lambda_L))
@@ -48,30 +48,26 @@ def main():
     mu = np.ones((Nx, Ny), dtype = np.float32) * mu0
 
     #PML parameters
-    pml_thickness = 20
+    pml_thickness = 10
     sigma_max = 1e10
-    sigma_x_vec, sigma_y_vec = pml_profile(sigma_max, pml_thickness, Nx, Ny)
+    sigma_x, sigma_y = pml_profile(sigma_max, pml_thickness, Nx, Ny)
 
     #main loop
     for n in range(nt):
         #add source
-        Ex[i_x_src][i_y_src][i_z_src] += gaussian_source(n, dt, sigma, omega_0)
+        Ez[i_x_src][i_y_src] += gaussian_source(n, dt, sigma, omega_0)
 
         Dz, Ez, Hx, Hy, Bx, By, Dz_old, Bx_old, By_old = update_equations(Dz, Ez, Hx, Hy, Bx, By,
             Dz_old, Bx_old, By_old,
             sigma_x, sigma_y, epsilon, mu,
             dt, dx, dy)
 
-        Ex_record[n] = Ex[i_x_prob, i_y_prob]
-
-    plot_final_fields(Ex, Ey, Nx, Ny)
     #plotting
-    t = np.arange(nt) * dt
-    plt.plot(t, Ex_record)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Ex at probe point')
-    plt.title('Field at Probe Point')
-    plt.grid(True)
+    fig, ax = plt.subplots()
+    ax.imshow(Ez, extent = [x_min,x_max,y_min,y_max])
+    square = patches.Rectangle((x_min+pml_thickness*dx, y_min+pml_thickness*dy), x_max-x_min-2*dx*pml_thickness, y_max-y_min-2*dy*pml_thickness,fc='none', ec='r')
+    ax.add_patch(square)
+    print(Ez)
     plt.show()
 
 def gaussian_source(n, dt, sigma, omega0):
@@ -80,18 +76,18 @@ def gaussian_source(n, dt, sigma, omega0):
     return np.exp(-((t_now - t0) / sigma)**2) * np.sin(omega0 * (t_now - t0))
 
 def sigma_profile(sigma_max, pml_thickness, distance):
+    poly_deg = 3
     return sigma_max * (distance/pml_thickness)**poly_deg
 
 def pml_profile(sigma_max, pml_thickness, Nx, Ny):
-    sigma_x = np.zeros(Nx)
-    sigma_y = np.zeros(Ny)
-    sigma_z = np.zeros(Nz)
+    sigma_x = np.zeros((Nx, Ny))
+    sigma_y = np.zeros((Nx, Ny))
     for i in range(pml_thickness):
-        sigma_x[i] = sigma_profile(sigma_max, pml_thickness, pml_thickness - i)
-        sigma_x[-1 - i] = sigma_profile(sigma_max, pml_thickness, pml_thickness - i)
+        sigma_x[i, :] = sigma_profile(sigma_max, pml_thickness, pml_thickness - i)
+        sigma_x[-1 - i, :] = sigma_profile(sigma_max, pml_thickness, pml_thickness - i)
     for j in range(pml_thickness):
-        sigma_y[j] = sigma_profile(sigma_max, pml_thickness, pml_thickness - j)
-        sigma_y[-1 - j] = sigma_profile(sigma_max, pml_thickness, pml_thickness - j)
+        sigma_y[:, j] = sigma_profile(sigma_max, pml_thickness, pml_thickness - j)
+        sigma_y[:, -1 - j] = sigma_profile(sigma_max, pml_thickness, pml_thickness - j)
     return sigma_x, sigma_y
 
 def update_equations(Dz, Ez, Hx, Hy, Bx, By,
@@ -99,78 +95,31 @@ def update_equations(Dz, Ez, Hx, Hy, Bx, By,
                      sigma_x, sigma_y, epsilon, mu,
                      dt, dx, dy):
 
-    Dz[1:-1, 1:-1] = ((1 - dt/2 * sigma_x[1:-1]) / (1 + dt/2 * sigma_x[1:-1])) * Dz[1:-1, 1:-1] + \
-        dt / (1 + dt/2 * sigma_x[1:-1]) / epsilon[1:-1] * (
-            (Hy[1:-2, 1:-1] - Hy[2:-1, 1:-1]) / dx -
-            (Hx[1:-1, 2:-1] - Hx[1:-1, 1:-2]) / dy
-        )
+    # Dz, Ez
+    Dz[1:-1, 1:-1] = ((1 - dt/2 * sigma_y[1:-1, 1:-1]) / (1 + dt/2 * sigma_x[1:-1, 1:-1])) * Dz[1:-1, 1:-1] + \
+        dt / (1 + dt/2 * sigma_x[1:-1, 1:-1]) / epsilon[1:-1, 1:-1] * \
+        ((Hy[1:, 1:-1] - Hy[:-1,1:-1]) / dx - (Hx[1:-1, 1:] - Hx[1:-1, :-1]) / dy)
+    Ez[1:-1, 1:-1] = ((1 - dt / 2 * sigma_x[1:-1, 1:-1]) / (1 + dt / 2 * sigma_y[1:-1, 1:-1])) * Ez[1:-1, 1:-1] + \
+        1 /  (1 + dt / 2 * sigma_y[1:-1, 1:-1]) * (Dz[1:-1, 1:-1] - Dz_old[1:-1, 1:-1])
 
-    Ez[1:-1, 1:-1, 1:-1] = ((1 - dt / 2 * sigma_y) / (1 + dt / 2 * sigma_y)) * Ez[1:-1, 1:-1, 1:-1] + \
-                           1 /  (1 + dt / 2 * sigma_y) * \
-                           (Dz[1:-1, 1:-1, 1:-1] - Dz_old[1:-1, 1:-1, 1:-1])
+    # Bx, Hx
+    sigma_y_Bx = 0.5 * (sigma_y[:, :-1] + sigma_y[:, 1:])
+    sigma_x_Hx = 0.5 * (sigma_x[:, :-1] + sigma_x[:, 1:])
+    mu_Hx = 0.5 * (mu[:, :-1] + mu[:, 1:])
+    Bx[:-1, :] = ((1 - dt/2 * sigma_y_Bx[:-1, :]) / (1 + dt/2 * sigma_y_Bx[:-1, :])) * Bx[:-1, :] - \
+        dt / (1 + dt/2 * sigma_y_Bx[:-1, :]) * mu_Hx[:-1, :] * (Ez[:-1, 1:] - Ez[:-1, :-1])
+    Hx[:-1, :] = Hx[:-1, :] + \
+            (1 + dt / 2 * sigma_x_Hx[:-1, :]) * Bx[:-1,:] - \
+            (1 - dt / 2 * sigma_x_Hx[:-1, :]) * Bx_old[:-1, :]
 
-    sigma_y_Bx = 0.5 * (sigma_y[:, :-1, :] + sigma_y[:, 1:, :])
-    sigma_y_Bx = 0.5 * (sigma_y_Bx[:, :, :-1] + sigma_y_Bx[:, :, 1:])
-    sigma_y_Bx = sigma_y_Bx[1:-1, :, :]
-
-    Bx[1:-1, 1:-1, 1:-1] = ((1 - dt/2 * sigma_y_Bx) / (1 + dt/2 * sigma_y_Bx)) * Bx[1:-1, 1:-1, 1:-1] + \
-        dt / (1 + dt/2 * sigma_y_Bx) * (
-            (Ey[1:-1, 1:-1, 1:] - Ey[1:-1, 1:-1, :-1]) / dz -
-            (Ez[1:-1, 1:, 1:-1] - Ez[1:-1, :-1, 1:-1]) / dy
-        )
-
-    sigma_z_By = 0.5 * (sigma_z[:-1, :, :] + sigma_z[1:, :, :])
-    sigma_z_By = 0.5 * (sigma_z_By[:, :, :-1] + sigma_z_By[:, :, 1:])
-    sigma_z_By = sigma_z_By[:, 1:-1, :]
-
-    By[1:-1, 1:-1, 1:-1] = ((1 - dt/2 * sigma_z_By) / (1 + dt/2 * sigma_z_By)) * By[1:-1, 1:-1, 1:-1] + \
-        dt / (1 + dt/2 * sigma_z_By) * (
-            (Ez[1:, 1:-1, 1:-1] - Ez[:-1, 1:-1, 1:-1]) / dx -
-            (Ex[1:-1, 1:-1, 1:] - Ex[1:-1, 1:-1, :-1]) / dz
-        )
-
-    sigma_z_Hx = 0.5 * (sigma_z[:, :-1, :] + sigma_z[:, 1:, :])
-    sigma_z_Hx = 0.5 * (
-                sigma_z_Hx[:, :, :-1] + sigma_z_Hx[:, :, 1:])
-
-    sigma_z_Hx_in = sigma_z_Hx[1:-1, :, :]
-
-    sigma_x_Hx = 0.5 * (sigma_x[:, :-1, :] + sigma_x[:, 1:, :])
-    sigma_x_Hx = 0.5 * (sigma_x_Hx[:, :, :-1] + sigma_x_Hx[:, :, 1:])
-    sigma_x_Hx_in = sigma_x_Hx[1:-1, :, :]
-
-    mu_Hx = 0.5 * (mu[:, :-1, :] + mu[:, 1:, :])
-    mu_Hx = 0.5 * (mu_Hx[:, :, :-1] + mu_Hx[:, :, 1:])
-    mu_Hx_in = mu_Hx[1:-1, :, :]
-
-    Hx[1:-1, 1:-1, 1:-1] = (
-            ((1 - dt / 2 * sigma_z_Hx_in) / (1 + dt / 2 * sigma_z_Hx_in)) * Hx[1:-1, 1:-1, 1:-1] +
-            1 / (mu_Hx_in * (1 + dt / 2 * sigma_z_Hx_in)) *
-            (
-                    (1 + dt / 2 * sigma_x_Hx_in) * Bx[1:-1, 1:-1, 1:-1] -
-                    (1 - dt / 2 * sigma_x_Hx_in) * Bx_old[1:-1, 1:-1, 1:-1]
-            )
-    )
-
-    sigma_x_Hy = 0.5 * (sigma_x[:-1, :, :] + sigma_x[1:, :, :])
-    sigma_x_Hy = 0.5 * (sigma_x_Hy[:, :, :-1] + sigma_x_Hy[:, :, 1:])
-    sigma_x_Hy_in = sigma_x_Hy[:, 1:-1, :]
-
-    sigma_y_Hy = 0.5 * (sigma_y[:-1, :, :] + sigma_y[1:, :, :])
-    sigma_y_Hy = 0.5 * (sigma_y_Hy[:, :, :-1] + sigma_y_Hy[:, :, 1:])
-    sigma_y_Hy_in = sigma_y_Hy[:, 1:-1, :]
-
-    mu_Hy = 0.5 * (mu[:-1, :, :] + mu[1:, :, :])
-    mu_Hy = 0.5 * (mu_Hy[:, :, :-1] + mu_Hy[:, :, 1:])
-    mu_Hy_in = mu_Hy[:, 1:-1, :]
-    Hy[1:-1, 1:-1, 1:-1] = (
-            ((1 - dt / 2 * sigma_x_Hy_in) / (1 + dt / 2 * sigma_x_Hy_in)) * Hy[1:-1, 1:-1, 1:-1] +
-            1 / (mu_Hy_in * (1 + dt / 2 * sigma_x_Hy_in)) *
-            (
-                    (1 + dt / 2 * sigma_y_Hy_in) * By[1:-1, 1:-1, 1:-1] -
-                    (1 - dt / 2 * sigma_y_Hy_in) * By_old[1:-1, 1:-1, 1:-1]
-            )
-    )
+    # By, Hy
+    sigma_x_By = 0.5 * (sigma_x[:-1, :] + sigma_x[1:, :])
+    sigma_y_Hy = 0.5 * (sigma_y[:-1, :] + sigma_y[1:, :])
+    mu_Hy = 0.5 * (mu[:-1, :] + mu[1:, :])
+    By[:, :-1] = ((1 - dt/2 * sigma_x_By[:, :-1]) / (1 + dt/2 * sigma_x_By[:, :-1])) * By[:, :-1] + \
+        dt / (1 + dt/2 * sigma_x_By[:, :-1]) / mu_Hy[:, :-1] * (Ez[1:, :-1] - Ez[:-1, :-1]) / dx
+    Hy[:, :-1] = Hy[:, :-1] + (1 + dt / 2 * sigma_y_Hy[:, :-1]) * By[:, :-1] - \
+        (1 - dt / 2 * sigma_y_Hy[:, :-1]) * By_old[:, :-1]
 
     Dz_old = np.copy(Dz)
     Bx_old = np.copy(Bx)
